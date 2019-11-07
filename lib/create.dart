b233c922c2f015d65ed76e131bf25109b2746536
zigitn <zigitn@5a6c.me>
2019-11-08 02:24:08 +0800

    完成修改和添加功能的基本复用

--- lib/create.dart
+++ lib/create.dart
@@ -13,27 +13,36 @@ class CreateNewContact extends StatefulWidget {
 class CreateNewContactState extends State<CreateNewContact> {
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String _name;
-  Item _phoneNumber;
+  Iterable<Item> _phoneNumbers;
   String _company;
   String _jobTitle;
+  String _identifier;
 
   void _forSubmitted() {
     var _form = this._formKey.currentState;
     if (_form.validate()) {
       _form.save();
-      ContactsService.addContact(Contact(
-        middleName: this._name,
-        phones: [this._phoneNumber],
-        company: this._company.isEmpty ? null : this._company,
-        jobTitle: this._jobTitle.isEmpty ? null : this._jobTitle,
-      ));
+      if (_identifier != null) {
+        ContactsService.addContact(Contact(
+          middleName: this._name,
+          phones: _phoneNumbers,
+          company: this._company.isEmpty ? null : this._company,
+          jobTitle: this._jobTitle.isEmpty ? null : this._jobTitle,
+        ));
+      } else {
+        widget.arguments["contact"].displayName = this._name;
+        widget.arguments["contact"].phones = this._phoneNumbers;
+        widget.arguments["contact"].company = this._company;
+        widget.arguments["contact"].jobTitle = this._jobTitle;
+        ContactsService.updateContact(widget.arguments["contact"]);
+      }
       Navigator.pushNamedAndRemoveUntil(context, "/", (route) => route == null);
     }
   }
 
   final FocusNode _nameFocus = FocusNode();
   final FocusNode _phoneNumberFocus = FocusNode();
-  final FocusNode _cpmpanyFocus = FocusNode();
+  final FocusNode _companyFocus = FocusNode();
   final FocusNode _jonTitleFocus = FocusNode();
 
   @override
@@ -44,6 +53,11 @@ class CreateNewContactState extends State<CreateNewContact> {
       _title = "新建联系人";
     } else {
       _title = "修改联系人";
+      this._identifier = widget.arguments["contact"].identifier;
+      this._name = widget.arguments["contact"].displayName;
+      this._phoneNumbers = widget.arguments["contact"].phones; // todo emails
+      this._company = widget.arguments["contact"].company;
+      this._jobTitle = widget.arguments["contact"].jobTitle;
     }
     return Scaffold(
       appBar: AppBar(
@@ -61,9 +75,17 @@ class CreateNewContactState extends State<CreateNewContact> {
             children: <Widget>[
               TextFormField(
                 focusNode: this._nameFocus,
-                onFieldSubmitted: (term){
-                  _fieldFocusChange(context, this._nameFocus, this._phoneNumberFocus);
+                onFieldSubmitted: (term) {
+                  _fieldFocusChange(
+                      context, this._nameFocus, this._phoneNumberFocus);
                 },
+                controller: TextEditingController.fromValue(TextEditingValue(
+                    text: '${this._name == null ? "" : this._name}',
+                    selection: TextSelection.fromPosition(
+                      TextPosition(
+                          affinity: TextAffinity.downstream,
+                          offset: '${this._name}'.length),
+                    ))),
                 textInputAction: TextInputAction.next,
                 decoration: InputDecoration(
                   labelText: '姓名',
@@ -79,8 +101,17 @@ class CreateNewContactState extends State<CreateNewContact> {
                 children: [
                   TextFormField(
                     focusNode: this._phoneNumberFocus,
-                    onFieldSubmitted: (term){
-                      _fieldFocusChange(context, this._phoneNumberFocus, this._cpmpanyFocus);
+                    keyboardType: TextInputType.phone,
+//                    controller: TextEditingController.fromValue(TextEditingValue(
+//                        text: '${this._phoneNumbers == null ? "" : this._phoneNumbers}',
+//                        selection: TextSelection.fromPosition(
+//                          TextPosition(
+//                              affinity: TextAffinity.downstream,
+//                              offset: '${this._phoneNumber}'.length),
+//                        ))),
+                    onFieldSubmitted: (term) {
+                      _fieldFocusChange(
+                          context, this._phoneNumberFocus, this._companyFocus);
                     },
                     textInputAction: TextInputAction.next,
                     decoration: InputDecoration(
@@ -90,15 +121,23 @@ class CreateNewContactState extends State<CreateNewContact> {
                       return val.isEmpty ? "电话不能为空" : null;
                     },
                     onSaved: (val) {
-                      this._phoneNumber = Item(value: val);
+                      this._phoneNumbers = [Item(value: val)];
                     },
                   ),
                 ],
               ),
               TextFormField(
-                focusNode: this._cpmpanyFocus,
-                onFieldSubmitted: (term){
-                  _fieldFocusChange(context, this._cpmpanyFocus, this._jonTitleFocus);
+                focusNode: this._companyFocus,
+                controller: TextEditingController.fromValue(TextEditingValue(
+                    text: '${this._company == null ? "" : this._company}',
+                    selection: TextSelection.fromPosition(
+                      TextPosition(
+                          affinity: TextAffinity.downstream,
+                          offset: '${this._company}'.length),
+                    ))),
+                onFieldSubmitted: (term) {
+                  _fieldFocusChange(
+                      context, this._companyFocus, this._jonTitleFocus);
                 },
                 textInputAction: TextInputAction.next,
                 decoration: InputDecoration(
@@ -110,7 +149,14 @@ class CreateNewContactState extends State<CreateNewContact> {
               ),
               TextFormField(
                 focusNode: this._jonTitleFocus,
-                onFieldSubmitted: (term){
+                controller: TextEditingController.fromValue(TextEditingValue(
+                    text: '${this._jobTitle == null ? "" : this._jobTitle}',
+                    selection: TextSelection.fromPosition(
+                      TextPosition(
+                          affinity: TextAffinity.downstream,
+                          offset: '${this._jobTitle}'.length),
+                    ))),
+                onFieldSubmitted: (term) {
                   _forSubmitted();
                 },
                 textInputAction: TextInputAction.done,
@@ -128,29 +174,10 @@ class CreateNewContactState extends State<CreateNewContact> {
     );
   }
 
-
-  _fieldFocusChange(BuildContext context, FocusNode currentFocus,FocusNode nextFocus) {
+  // 焦点切换
+  _fieldFocusChange(
+      BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
     currentFocus.unfocus();
     FocusScope.of(context).requestFocus(nextFocus);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  void didUpdateWidget(CreateNewContact oldWidget) {
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
   }
 }
